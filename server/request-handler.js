cae1030c85d62c8420d68c7373f8194bb3f7d8b0
Neco-Murphy <stogamori@gmail.com>
2014-09-29 18:57:48 -0700

    Completed core requirements.

--- server/request-handler.js
+++ server/request-handler.js
@@ -53,6 +53,49 @@ exports.handleRequest = function(request, response) {
   }
 };
 
+exports.handler = function(request, response){
+  /* the 'request' argument comes from nodes http module. It includes info about the
+  request - such as what URL the browser is requesting. */
+
+  /* Documentation for both request and response can be found at
+   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
+  console.log("Serving request type " + request.method + " for url " + request.url);
+
+  var statusCode = 200;
+
+  /* Without this line, this server wouldn't work. See the note
+   * below about CORS. */
+  var headers = defaultCorsHeaders;
+
+  headers['Content-Type'] = "text/plain";
+  /* .writeHead() tells our server what HTTP status code to send back */
+  //response.writeHead(statusCode, headers);
+
+  /* Make sure to always call response.end() - Node will not send
+   * anything back to the client until you do. The string you pass to
+   * response.end() will be the body of the response - i.e. what shows
+   * up in the browser.*/
+
+
+
+  if(request.method === "POST"){
+    request.on('data', function (chunk) {
+      data.results.push(JSON.parse(chunk));
+    });
+    statusCode = 201;
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify(data));
+  } else if (request.method === "GET" && request.url.search("/classes/room") !== -1) {
+    statusCode = 200;
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify(data))
+  } else {
+    statusCode = 404;
+    response.writeHead(statusCode, headers);
+    response.end();
+  }
+};
+
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
  * This CRUCIAL code allows this server to talk to websites that
  * are on different domains. (Your chat client is running from a url
